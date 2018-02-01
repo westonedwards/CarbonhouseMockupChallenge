a5155622dd2a483f3fa03aabb30027d877f289b8
Weston <westonedwards@gmail.com>
2018-02-01 14:39:36 -0500

    Update app.js

--- app.js
+++ app.js
@@ -15,10 +15,10 @@ app.use((req, res, next) => {
 });
  
 var client = new Twitter({
-  consumer_key: 'S8VKbhaLXZqn6TsbCzzi2di2j',
-  consumer_secret: 'WZACVfCgSXpMqMv2JuvmY4IVz2cqB86Y1jugLCRdDTZgjAjXKH',
-  access_token_key: '47637346-4vF7DIUUdH27jQPWCXyvGXaZ42n0bZNPb2SGJVLa0',
-  access_token_secret: 'JYacuQ0YuSUO6E7Elfpji6oraQjXKrD61e8BdrI7WeWgE'
+  consumer_key: '***',
+  consumer_secret: '***',
+  access_token_key: '***',
+  access_token_secret: '***'
 });
  
 var params = {screen_name: 'carbonhouse'};
